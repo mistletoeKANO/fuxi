df6b7a2c533cad5db4b835e80809b9c0412fa5dd
Mono <1649881580@qq.com>
2022-06-22 21:44:14 +0800

    Fix

--- FuXi/Editor/Fx_EditorConfigs.cs
+++ FuXi/Editor/Fx_EditorConfigs.cs
@@ -2,9 +2,9 @@
 {
     public static class Fx_EditorConfigs
     {
-        public static readonly string DefaultAssetName = "FuXi Asset.asset";
-        public static readonly string FirstPackageName = "BuiltinPackage";
-        public static readonly string AdditionPackageName = "AdditionPackage";
+        public static readonly string DefaultAssetName = "FuXiAsset.asset";
+        public static readonly string FirstPackageName = "Builtin";
+        public static readonly string AdditionPackageName = "Addition";
         public static readonly string SettingName = "Settings";
 
         public static readonly string AboutURL = "https://github.com/mistletoeKANO/fuxi";
